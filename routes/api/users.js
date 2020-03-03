ecc24f3e522d637f55430204739525ddcfcd2d15
tharindu97 <tharindukavishna@gmail.com>
2020-03-03 13:19:04 +0530

    jwt_verify

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
@@ -43,7 +45,19 @@ router.post('/',[
 
         await user.save();
 
-        res.send('Users Registered');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            { expiresIn: 360000 }, 
+            (err, token)=>{
+                if(err) throw err;
+                res.json({ token });
+            });
     } catch (err) {
         console.log(err.message);
         res.status(500).send('Server error');
