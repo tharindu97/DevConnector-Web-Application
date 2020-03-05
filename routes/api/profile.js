592160e2b75fa155770f74d034365d05f37ae0b2
tharindu97 <tharindukavishna@gmail.com>
2020-03-05 21:17:21 +0530

    get_github_repo

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -325,7 +325,25 @@ router.get('/user/:user_id', async(req,res) => {
 
     router.get('/github/:username', (req,res) => {
         try {
-            
+            const options = {
+                uri: `https://api.github.com/users/${
+                    req.params.username
+                }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+                    'githubClientId'
+                )}&client_secret=${config.get('githubSecrect')}`,
+                method: 'GET',
+                headers: { 'user-agent' : 'node.js' }
+            };
+
+            request(options, (error, response, body) =>{
+                if(error) console.error(error);
+
+                if(response.statusCode !== 200){
+                    res.status(404).json({ msg: 'No Github profile found' });
+                }
+
+                res.json(JSON.parse(body));
+            });
         } catch (err) {
             console.log(err.message);
             res.status(500).send('Server Error');
