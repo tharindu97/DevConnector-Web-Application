70fd41f444b9a03dde32c8c9fcb21120bffb6bb2
tharindu97 <tharindukavishna@gmail.com>
2020-05-09 12:13:48 +0530

    09_2_completed

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -8,5 +8,16 @@ import {
  
 // Get current users profile
 export const getCurrentProfile = () => async dispatch =>{
-    
+    try {
+        const res = await axios.get('/api/profile/me');
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
 }
