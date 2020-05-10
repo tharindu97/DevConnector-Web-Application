44d3fb083c14ef9e6ef6213df4429fb9f05ee031
tharindu97 <tharindukavishna@gmail.com>
2020-05-10 21:43:28 +0530

    9_5_start

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -23,4 +23,36 @@ export const getCurrentProfile = () => async dispatch =>{
             }
         });
     }
-}
+};
+
+// create or update profile
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const res = await axios.post('/api/profile', formData, config);
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert(edit ? 'Profile Update' : 'Profile Created'));
+        if(!edit){
+            history.push('/dashboard');
+        }
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { 
+                msg: err.response.statusText, 
+                status: err.response.status 
+            }
+        });
+    }
+};
+
+// 5 11
